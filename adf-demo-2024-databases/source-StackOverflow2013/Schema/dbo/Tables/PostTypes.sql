036e1de03f00e7553973c7da149ce60e27d376ba
DeirdreOLeary <deirdre.e.oleary@gmail.com>
2024-06-26 17:07:22 +0200

    Added source tables.

---
+++ adf-demo-2024-databases/source-StackOverflow2013/Schema/dbo/Tables/PostTypes.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[PostTypes] (
+    [Id]   INT           IDENTITY (1, 1) NOT NULL,
+    [Type] NVARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_PostTypes_Id] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
