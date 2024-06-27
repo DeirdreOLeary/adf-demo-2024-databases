2350a42f254b53a70503f91d4883d11e377d9d06
DeirdreOLeary <deirdre.e.oleary@gmail.com>
2024-06-27 12:02:05 +0200

    Created stg.Source_Users & stg.Source_Badges tables to hold source data from StackOverflow database.

---
+++ adf-demo-2024-databases/destination-sqldb-demo-dw-2024/Schema/stg/Tables/Source_Badges.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [stg].[Source_Badges] (
+    [Id]     INT           NOT NULL,
+    [Name]   NVARCHAR (40) NOT NULL,
+    [UserId] INT           NOT NULL,
+    [Date]   DATETIME      NOT NULL
+);
---
+++ adf-demo-2024-databases/destination-sqldb-demo-dw-2024/Schema/stg/Tables/Source_Users.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [stg].[Source_Users] (
+    [Id]          INT,
+    [DisplayName] NVARCHAR (40) NOT NULL,
+    [Reputation]  INT           NOT NULL
+);
