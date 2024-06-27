587c778621c5b2be449c7d2d4ea339a764b75f70
DeirdreOLeary <deirdre.e.oleary@gmail.com>
2024-06-27 12:15:26 +0200

    Created stg.Source_Posts & stg.Source_PostTypes tables to hold source data from StackOverflow database.

---
+++ adf-demo-2024-databases/destination-sqldb-demo-dw-2024/Schema/stg/Tables/Source_Posts.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [stg].[Source_Posts] (
+    [Key]          INT      NOT NULL,
+    [CommentCount] INT      NULL,
+    [CreationDate] DATETIME NOT NULL,
+    [OwnerUserId]  INT      NULL,
+    [Score]        INT      NOT NULL,
+    [ViewCount]    INT      NOT NULL,
+);
