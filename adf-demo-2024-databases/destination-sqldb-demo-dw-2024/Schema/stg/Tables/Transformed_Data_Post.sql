c8c1b042763226debcb90c671fbf793363c6844b
DeirdreOLeary <deirdre.e.oleary@gmail.com>
2024-06-27 12:06:39 +0200

    Created stg.Transformed_Data_Post table to hold post data that has been transformed to the factPost schema.

---
+++ adf-demo-2024-databases/destination-sqldb-demo-dw-2024/Schema/stg/Tables/Transformed_Data_Post.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [stg].[Transformed_Data_Post] (
+    [PostKey]      INT          NOT NULL,
+    [OwnerUserId]  INT          NOT NULL,
+    [CreationDate] DATETIME2(2) NOT NULL,
+    [Score]        INT          NOT NULL,
+    [ViewCount]    INT          NOT NULL,
+    [CommentCount] INT          NULL,
+    [IsUpdate]     BIT          DEFAULT 0 NOT NULL
+    CONSTRAINT [UC_Transformed_Data_Post] UNIQUE ([PostKey])
+);
