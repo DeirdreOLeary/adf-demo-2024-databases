8051782a4521505b8f92960757c6c7e2c183b21c
DeirdreOLeary <deirdre.e.oleary@gmail.com>
2024-06-27 12:00:20 +0200

    Created stg.Transformed_Data_User table to hold user & badge data that has been transformed to the dimUser schema.

---
+++ adf-demo-2024-databases/destination-sqldb-demo-dw-2024/Schema/stg/Tables/Transformed_Data_User.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [stg].[Transformed_Data_User] (
+    [UserKey]                INT           NOT NULL,
+    [DisplayName]            NVARCHAR (40) NOT NULL,
+    [Reputation]             INT           NOT NULL,
+    [LatestBadgeName]        NVARCHAR (40) NULL,
+    [DateLatestBadgeAwarded] DATETIME2(2)  NULL,
+    [Version]                INT           DEFAULT 1 NOT NULL,
+    [IsReplacement]          BIT           DEFAULT 0 NOT NULL
+    CONSTRAINT [UC_Transformed_Data_User] UNIQUE ([UserKey])
+);
