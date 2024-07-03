11cb21008155c014059ce9f9f003e68cb4dadb42
DeirdreOLeary <deirdre.e.oleary@gmail.com>
2024-07-03 11:24:01 +0200

    Added stg.ADF_Truncate_Staging_Tables stored proc to truncate staging tables (except the dim & fact tables).

---
+++ adf-demo-2024-databases/destination-sqldb-demo-dw-2024/Schema/stg/Stored Procedures/ADF_Truncate_Staging_Tables.sql
@@ -0,0 +1,22 @@
+﻿/***********************************************************************************************
+Purpose:	This stored proc is called by the Azure Data Factory ETL process to truncate all 
+			staging Source & Transformed_Data tables in the data warehouse's stg schema.
+
+Pipeline:	
+
+Activity:	
+
+***********************************************************************************************/
+CREATE PROCEDURE [stg].[ADF_Truncate_Staging_Tables]
+AS
+	/* Truncate all Source tables */
+	TRUNCATE TABLE [stg].[Source_Badges];
+	TRUNCATE TABLE [stg].[Source_Posts];
+	TRUNCATE TABLE [stg].[Source_PostTypes];
+	TRUNCATE TABLE [stg].[Source_Users];
+
+	/* Truncate all Transformed_Data tables */
+	TRUNCATE TABLE [stg].[Transformed_Data_Post];
+	TRUNCATE TABLE [stg].[Transformed_Data_User];
+
+GO
