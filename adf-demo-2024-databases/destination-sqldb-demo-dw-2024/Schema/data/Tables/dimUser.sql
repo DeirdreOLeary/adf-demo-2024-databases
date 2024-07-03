cd63434016522ee0b8b450c9b04e4c12eea30fd6
DeirdreOLeary <deirdre.e.oleary@gmail.com>
2024-07-03 11:07:49 +0200

    Added data schema, dim & fact tables. This schema is the production one that is accessed by data analysts.

---
+++ adf-demo-2024-databases/destination-sqldb-demo-dw-2024/Schema/data/Tables/dimUser.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [data].[dimUser] (
+    [UserId]                 INT            NOT NULL,   /* Surrogate key */
+    [VersionStartDate]       DATETIME2(2)   NOT NULL,
+	[VersionEndDate]         DATETIME2(2)   NOT NULL,
+	[Version]                INT            NOT NULL,
+	[IsActive]               BIT            NOT NULL,
+    [UserKey]                INT            NOT NULL,   /* Natural key */
+    [DisplayName]            NVARCHAR (40)  NOT NULL,
+    [Reputation]             INT            NOT NULL,
+    [LatestBadgeName]        NVARCHAR (40)  NULL,
+    [DateLatestBadgeAwarded] DATETIME2(2)   NULL,
+);
---
+++ adf-demo-2024-databases/destination-sqldb-demo-dw-2024/Schema/data/Tables/factPost.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [data].[factPost] (
+    [PostId]          INT           NOT NULL,   /* Surrogate key */
+    [DateLastUpdated] DATETIME2(2)  NOT NULL,
+    [PostKey]         INT           NOT NULL,   /* Natural key */
+    [OwnerUserId]     INT           NOT NULL,
+    [CreationDate]    DATETIME2(2)  NOT NULL,
+    [Score]           INT           NOT NULL,
+    [ViewCount]       INT           NOT NULL,
+    [CommentCount]    INT           NULL,
+);
